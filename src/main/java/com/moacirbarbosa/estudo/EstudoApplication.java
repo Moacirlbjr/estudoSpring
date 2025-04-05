345120f2555f4d76022a06fa6d7177ae63432824
Moacir Barbosa <moacirlbjr@hotmail.com>
2025-04-05 09:30:04 -0300

    Criando operacao de instanciacao

--- src/main/java/com/moacirbarbosa/estudo/EstudoApplication.java
+++ src/main/java/com/moacirbarbosa/estudo/EstudoApplication.java
@@ -1,13 +1,34 @@
 package com.moacirbarbosa.estudo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudoApplication {
+import com.moacirbarbosa.estudo.domain.Categoria;
+import com.moacirbarbosa.estudo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
