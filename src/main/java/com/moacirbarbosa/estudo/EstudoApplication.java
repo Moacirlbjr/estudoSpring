5e54f1482aed738fcb5ca6e12ca76696a5810c35
Moacir Barbosa <moacirlbjr@hotmail.com>
2025-04-05 23:40:10 -0300

    Enuns tipo difenrente - Cliente e Endereco

--- src/main/java/com/moacirbarbosa/estudo/EstudoApplication.java
+++ src/main/java/com/moacirbarbosa/estudo/EstudoApplication.java
@@ -1,7 +1,7 @@
 package com.moacirbarbosa.estudo;
 
 import java.util.Arrays;
-
+import com.moacirbarbosa.estudo.resources.CategoriaResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,15 +9,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.moacirbarbosa.estudo.domain.Categoria;
 import com.moacirbarbosa.estudo.domain.Cidade;
+import com.moacirbarbosa.estudo.domain.Cliente;
+import com.moacirbarbosa.estudo.domain.Endereco;
 import com.moacirbarbosa.estudo.domain.Estado;
 import com.moacirbarbosa.estudo.domain.Produto;
+import com.moacirbarbosa.estudo.domain.enuns.TipoCliente;
 import com.moacirbarbosa.estudo.repositories.CategoriaRepository;
 import com.moacirbarbosa.estudo.repositories.CidadeRepository;
+import com.moacirbarbosa.estudo.repositories.ClienteRepository;
+import com.moacirbarbosa.estudo.repositories.EnderecoRepository;
 import com.moacirbarbosa.estudo.repositories.EstadoRepository;
 import com.moacirbarbosa.estudo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class EstudoApplication implements CommandLineRunner{
+
+    private final CategoriaResource categoriaResource;
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -30,6 +37,16 @@ public class EstudoApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+    EstudoApplication(CategoriaResource categoriaResource) {
+        this.categoriaResource = categoriaResource;
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
@@ -67,7 +84,19 @@ public class EstudoApplication implements CommandLineRunner{
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3)); 
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","34948294834",TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("83481345","34533234"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores","300","apt 303","Jardim","38223804",cli1,c1);
+		Endereco e2 = new Endereco(null,"Av Matos","105","Sala 800","Centro","38777012",cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));		
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
---
+++ src/main/java/com/moacirbarbosa/estudo/domain/Cliente.java
@@ -0,0 +1,175 @@
+package com.moacirbarbosa.estudo.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import com.moacirbarbosa.estudo.domain.enuns.TipoCliente;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+@Entity
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private Integer tipo;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCod();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the cpfOuCnpj
+	 */
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	/**
+	 * @param cpfOuCnpj the cpfOuCnpj to set
+	 */
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	/**
+	 * @return the tipo
+	 */
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	/**
+	 * @return the enderecos
+	 */
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	/**
+	 * @param enderecos the enderecos to set
+	 */
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	/**
+	 * @return the telefones
+	 */
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	/**
+	 * @param telefones the telefones to set
+	 */
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
